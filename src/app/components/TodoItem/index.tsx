3a167ccd588b230ba675d8e4327c8323c4ddd51e
limEnough <limenough@github.com>
2024-03-08 23:59:07 +0900

    feat: add edit type to input component

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
 import Block from '../Block';
 import Checkbox from '../Checkbox';
+import CircleButton from '../Button/CircleButton';
+import TodoInput from '../TodoInput';
 
 const Box = styled.div`
   display: flex;
   align-items: center;
   padding: 15px 25px;
   width: 100%;
+  min-height: 67px;
   font-size: 1.2em;
   border-bottom: 1px solid #eee;
+
+  & > .delete-button {
+    display: none;
+  }
+
+  &:hover {
+    & > .delete-button {
+      display: flex;
+    }
+  }
 `;
 
 const TodoContent = styled.span<{ checked: boolean }>`
@@ -26,9 +39,32 @@ const TodoContent = styled.span<{ checked: boolean }>`
 export default function TodoItem({ todo }: { todo: TodoItem }) {
   return (
     <Box>
-      <Checkbox checked={todo.completed}></Checkbox>
-      <Block marginLeft="10px"></Block>
-      <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+      {/* 콘텐츠 */}
+      <div style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <Checkbox checked={todo.completed}></Checkbox>
+        <Block marginLeft="10px"></Block>
+        {todo.editing ? (
+          <TodoInput></TodoInput>
+        ) : (
+          <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+        )}
+      </div>
+
+      {/* 액션 */}
+      <CircleButton
+        onClick={() => {}}
+        className="delete-button"
+        Icon={() => (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+          </svg>
+        )}
+      ></CircleButton>
     </Box>
   );
 }
