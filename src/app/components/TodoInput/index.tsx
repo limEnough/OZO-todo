189990fa239dc715573c6d1a39abb3f0fae4fce8
limEnough <limenough@github.com>
2024-03-08 23:59:07 +0900

    feat: add edit type to input component

--- src/app/components/TodoInput/index.tsx
+++ src/app/components/TodoInput/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Box = styled.div`
+const Box = styled.div<{ isEditing?: boolean }>`
   display: flex;
   align-items: center;
-  padding: 15px 25px;
+  padding: ${props => (props.isEditing ? '5px 0px' : '15px 25px')};
   width: 100%;
   font-size: 1.2em;
   border-bottom: 1px solid #eee;
@@ -18,26 +18,44 @@ const Input = styled.input`
 
 export default function TodoInput({
   setTodoList,
+  isEditing,
+  editContent,
+  editModeTodo,
+  editTodo,
 }: {
-  setTodoList: (todo: TodoItem) => void;
+  setTodoList?: (todo: TodoItem) => void;
+  isEditing?: boolean;
+  editContent?: string;
+  editModeTodo?: () => void;
+  editTodo?: (content: string) => void;
 }) {
   const [content, setContent] = React.useState<string>('');
   return (
-    <Box>
+    <Box isEditing={isEditing}>
       <Input
         placeholder="할 일을 입력해주세요."
         value={content}
+        onBlur={e => {
+          if (e.currentTarget === e.target) {
+            editTodo && editTodo(content);
+          }
+        }}
         onChange={e => setContent(e.target.value)}
         onKeyPress={e => {
           if (content === '') return;
           if (e.key !== 'Enter' && e.key !== 'NumpadEnter') return;
-          setTodoList({
-            id: '0',
-            content: content,
-            completed: false,
-            editing: false,
-          });
-          setContent('');
+          if (isEditing) {
+            editTodo && editTodo(content);
+          } else {
+            setTodoList &&
+              setTodoList({
+                id: '0',
+                content: content,
+                completed: false,
+                editing: false,
+              });
+            setContent('');
+          }
         }}
       ></Input>
     </Box>
